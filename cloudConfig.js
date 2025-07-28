051762dfc9372e48c647cefe962ce00e5bac59b7
Souvik Majee <souvikmajee039@gmail.com>
2025-07-28 22:55:48 +0530

    cloudinary fixed

--- cloudConfig.js
+++ cloudConfig.js
@@ -13,7 +13,11 @@ const storage = new CloudinaryStorage({
     params: {
       folder: 'swiftRooms_DEV',
       allowedFormats:["png","jpg","jpeg"],
-      public_id: (req, file) => 'computed-filename-using-request',
+      public_id: (req, file) => {
+        const timestamp = Date.now();
+        const random = Math.floor(Math.random() * 10000);
+        return `image_${timestamp}_${random}`;
+      }      
     },
   });
 
